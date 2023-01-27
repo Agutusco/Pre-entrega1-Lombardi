a7389b0f4f1d4d8594c78c79b7ce5c8430d66a02
Agustín Lombardi <agutusco@gmail.com>
2023-01-27 16:09:43 -0300

    Agrego eventos

---
+++ clase_1/Curso Agus JS/productos.js
@@ -0,0 +1,53 @@
+// productos videojuegos
+
+let stockproductos = [
+    {
+        id: 1, nombre: "Call of Duty Vanguard", precio: 2400, desc:"El videojuego de disparos en primera persona más conocido del mundo", img: "./assets/call-of-duty-vanguard.png"
+    },
+    {
+        id: 2, nombre: "Fifa 23", precio: 4000, desc:"La nueva entrega del mejor videojuego de futbol vale toda la pena", img: "./assets/D_NQ_NP_605970-MLA51948617706_102022-O.jpg"
+    },
+    {
+        id: 3, nombre: "Lego Marvel", precio: 2400, desc: "Un juego tranquilo para pasar el tiempo con amigos desbloqueando personajes.", img: "./assets/D_NQ_NP_698905-MLA25113696605_102016-O.webp"
+    },
+    {
+        id: 4 , nombre: "UFC 4", precio: 2800, desc:"Un videojuego de artes marciales muy conocido. Si pelear te gusta mucho es el juego indicado.", img: "./assets/D_NQ_NP_786285-MLA44627637605_012021-O.jpg"
+    },
+    {
+        id: 5, nombre: "The Last OF Us", precio: 3100, desc: "El mejor juego de zombies con una historia que atrapa a todas las edades.", img: "./assets/795-7952132_last-of-us-ps4-box.png"
+    },
+    {
+        id: 6, nombre: "Tom Clancy´s Rainbow Six Siege", precio: 4000, desc:"Dos equipos, 5 contra 5, muchos especialistas y mucha estrategia", img: "./assets/Rainbow_Six_Siege.jpg"
+    },
+    {
+        id:7, nombre: "Assasin´s Creed Valhalla", precio: 4000, desc: "Vikingos, asesinos y acción, todo lo que te trae este juego es asombroso.", img:"./assets/EDtkdijFRwbmGKk1G9lrDoEF.webp"
+    },
+    {
+        id: 8, nombre: "Red Dead Redemption 2", precio: 3800, desc:"Si los caballos y las armas te gustan, este es el juego indicado.", img: "./assets/h.jpg"
+    },
+    {
+        id: 9, nombre: "GTA 5", precio: 3000, desc:"El mitico juego de acción y aventura con muchas misiones para hacer junto a distintos personajes.", img: "./assets/gta.jpg"
+    }
+]
+
+// productos consolas
+let stockconsolas =[
+    {
+        id: 10, nombre: "Control Playstation 4", precio: 17000, desc:"Completamente nuevo para jugar con la consola de Playstation 4", desc2:"Colores negro o blanco", desc3:"Viene con cargador", img: "./assets/michal-ilenda-UzwsrV3bFdU-unsplash.jpg"
+    },
+    {
+        id:11, nombre: "Playstation 4 ", precio: 180000, desc:"La consola más famosa de la historia del gaming nunca puede faltar", desc2: "Viene con un control", desc3:"Viene con el Uncharted 4", img: "./assets/D_NQ_NP_649238-MLA47385331667_092021-V.jpg"
+    },
+    {
+        id:12, nombre: "Nintendo Switch", precio: 220000, desc:"Se puede jugar multijugador y viene con el modo TV, el modo semiportátil y el modo portátil", desc2:"Viene con todos sus cargadores", desc3:"Viene con los dos joysticks", img: "./assets/consola-nintendo-switch-neon-v11.jpg"
+    },
+    {
+        id: 13, nombre: "Control Xbox One", precio: 17000, desc: "Completamente nuevo para jugar a la Xbox One con amigos", desc2: "Colores negro o blanco", desc3:"Viene con cargador", img: "./assets/mika-baumeister-G_54j8aD43U-unsplash.jpg"
+    },
+    {
+        id: 14, nombre:"Xbox One", precio: 210000, desc:"Otro tipo de consola igual de divertida que la PS4", desc2: "Viene con un control", desc3: "Colores negro o blanco", img: "./assets/la-nueva-xbox-one-s-consola-de-videojuegos.avif"
+    },
+    {
+        id: 15, nombre: "Consola Arcade", precio: 300000, desc:"La mejor consola para revivir los 80S y 90S. Incluye juegos modernos también", desc2:"Viene con 500 juegos", desc3:"El diseño se decide en la tienda", img: "./assets/images.jpg"
+    }
+]
--- clase_1/Curso Agus JS/script.js
+++ clase_1/Curso Agus JS/script.js
@@ -1,195 +1,244 @@
-// Costo de los productos de videojuegos
-const costoproducto1 = 2400
-const costoproducto2 = 4000
-const costoproducto3 = 2400
-const costoproducto4 = 2800
-const costoproducto5 = 3100
-const costoproducto6 = 4000
-const costoproducto7 = 4000
-const costoproducto8 = 3800
-const costoproducto9 = 3000
+// // Costo de los productos de videojuegos
+// const costoproducto1 = 2400
+// const costoproducto2 = 4000
+// const costoproducto3 = 2400
+// const costoproducto4 = 2800
+// const costoproducto5 = 3100
+// const costoproducto6 = 4000
+// const costoproducto7 = 4000
+// const costoproducto8 = 3800
+// const costoproducto9 = 3000
 
 
-let cantidadproducto1
-let cantidadproducto2
-let cantidadproducto3
-let cantidadproducto4
-let cantidadproducto5
-let cantidadproducto6
-let cantidadproducto7
-let cantidadproducto8
-let cantidadproducto9
+// let cantidadproducto1
+// let cantidadproducto2
+// let cantidadproducto3
+// let cantidadproducto4
+// let cantidadproducto5
+// let cantidadproducto6
+// let cantidadproducto7
+// let cantidadproducto8
+// let cantidadproducto9
+const tiendadevidejuegos = document.getElementById("tiendavideojuegos")
+stockproductos.forEach((producto) => {
+    const div = document.createElement("div")
+    div.innerHTML = `
+    <div class="contenido">
+    <div class="card border-info mb-3" style="width: 18rem;">
+    <img src="${producto.img}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${producto.nombre}</h5>
+    <p class="card-text">${producto.desc}<br> <strong>${producto.precio}$</strong></p>
+    <a href="" class="btn btn-primary">Agregar al carrito</a>
+    </div>
+    </div>
+    `
+    tiendadevidejuegos.appendChild(div)
+})
+
+const tiendadeconsolas = document.getElementById("tiendadecontroles")
+stockconsolas.forEach((producto) => {
+    const div = document.createElement("div")
+    div.innerHTML = `
+    <div class="contenido">
+    <div class="card border-info mb-3" style="width: 18rem;">
+    <img src="${producto.img}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${producto.nombre}</h5>
+    <p class="card-text">${producto.desc}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+    <li class="list-group-item">${producto.desc2}</li>
+    <li class="list-group-item">${producto.desc3}</li>
+    <li class="list-group-item"><strong>${producto.precio}</strong></li>
+    </ul>
+    <div class="card-body">
+    <a href="" class="btn btn-secondary" id="BtnCarrito">Agregar al carrito</a>
+    </div>
+</div>
+</div>
+    `
+    tiendadeconsolas.appendChild(div)
+})
 
 
-// Cuantos productos quiere llevarse
-function catalogo() {
-do{ 
-    cantidadproducto1 = parseInt(prompt("Ingrese cuantos juegos de COD quiere llevarse"))
-    cantidadproducto2 = parseInt(prompt("Ingrese cuantos juegos de Fifa 23 quiere llevarse"))
-    cantidadproducto3 = parseInt(prompt("Ingrese cuantos juegos de Lego Marvel quiere llevarse"))
-    cantidadproducto4 = parseInt(prompt("Ingrese cuantos juegos de UFC 4 quiere levarse"))
-    cantidadproducto5 = parseInt(prompt("Ingrese cuantos juegos de The Last of Us quiere llevarse"))
-    cantidadproducto6 = parseInt(prompt("Ingrese cuantos juegos de R6 quiere llevarse"))
-    cantidadproducto7 = parseInt(prompt("Ingrese cuantos juegos de Assasin´s Creed quiere llevarse"))
-    cantidadproducto8 = parseInt(prompt("Ingrese cuantos juegos de Red Dead Redemption 2 quiere llevarse"))
-    cantidadproducto9 = parseInt(prompt("Ingrese cuantos juegos de GTA quiere llevarse"))
-}while (cantidadproducto1 == 0 && cantidadproducto2 == 0 && cantidadproducto3 == 0 && cantidadproducto4 == 0 && cantidadproducto5 == 0 && cantidadproducto6 == 0 && cantidadproducto7 == 0 && cantidadproducto8 == 0 && cantidadproducto9 == 0) {
-    if(cantidadproducto1 == 0 && cantidadproducto2 == 0 && cantidadproducto3 == 0 && cantidadproducto4 == 0 && cantidadproducto5 == 0 && cantidadproducto6 == 0 && cantidadproducto7 == 0 && cantidadproducto8 == 0 && cantidadproducto9 == 0){
-        alert(" No selecciono ningun articulo ")
-                }
-        }
-    }catalogo()
+// // Cuantos productos quiere llevarse
+// function catalogo() {
+// do{ 
+//     cantidadproducto1 = parseInt(prompt("Ingrese cuantos juegos de COD quiere llevarse"))
+//     cantidadproducto2 = parseInt(prompt("Ingrese cuantos juegos de Fifa 23 quiere llevarse"))
+//     cantidadproducto3 = parseInt(prompt("Ingrese cuantos juegos de Lego Marvel quiere llevarse"))
+//     cantidadproducto4 = parseInt(prompt("Ingrese cuantos juegos de UFC 4 quiere levarse"))
+//     cantidadproducto5 = parseInt(prompt("Ingrese cuantos juegos de The Last of Us quiere llevarse"))
+//     cantidadproducto6 = parseInt(prompt("Ingrese cuantos juegos de R6 quiere llevarse"))
+//     cantidadproducto7 = parseInt(prompt("Ingrese cuantos juegos de Assasin´s Creed quiere llevarse"))
+//     cantidadproducto8 = parseInt(prompt("Ingrese cuantos juegos de Red Dead Redemption 2 quiere llevarse"))
+//     cantidadproducto9 = parseInt(prompt("Ingrese cuantos juegos de GTA quiere llevarse"))
+// }while (cantidadproducto1 == 0 && cantidadproducto2 == 0 && cantidadproducto3 == 0 && cantidadproducto4 == 0 && cantidadproducto5 == 0 && cantidadproducto6 == 0 && cantidadproducto7 == 0 && cantidadproducto8 == 0 && cantidadproducto9 == 0) {
+//     if(cantidadproducto1 == 0 && cantidadproducto2 == 0 && cantidadproducto3 == 0 && cantidadproducto4 == 0 && cantidadproducto5 == 0 && cantidadproducto6 == 0 && cantidadproducto7 == 0 && cantidadproducto8 == 0 && cantidadproducto9 == 0){
+//         alert(" No selecciono ningun articulo ")
+//                 }
+//         }
+//     }catalogo()
 
 
-let total1 = 0
-function sumar() {
-    total1 = (cantidadproducto1 * costoproducto1) + (cantidadproducto2 * costoproducto2) + (cantidadproducto3 * costoproducto3) + (cantidadproducto4 * costoproducto4) + (cantidadproducto5 * costoproducto5) + (cantidadproducto6 * costoproducto6) + (cantidadproducto7 * costoproducto7) + (cantidadproducto8 * costoproducto8) + (cantidadproducto9 * costoproducto9)
-    alert(" Tu costo total es de " + total1 )
-}
-sumar()
-console.log(total1)
+// let total1 = 0
+// function sumar() {
+//     total1 = (cantidadproducto1 * costoproducto1) + (cantidadproducto2 * costoproducto2) + (cantidadproducto3 * costoproducto3) + (cantidadproducto4 * costoproducto4) + (cantidadproducto5 * costoproducto5) + (cantidadproducto6 * costoproducto6) + (cantidadproducto7 * costoproducto7) + (cantidadproducto8 * costoproducto8) + (cantidadproducto9 * costoproducto9)
+//     alert(" Tu costo total es de " + total1 )
+// }
+// sumar()
+// console.log(total1)
 
 
-class Producto{
-    constructor(nombre,precio,){
-        this.nombre = nombre
-        this.precio = parseInt(precio)
-    }
-    enviar(){
-        this.precio = this.precio + 210
-    }
-    pago(){
-        this.precio = this.precio - 500
-    }
-}
-const productos = []
-productos.push(new Producto( " COD ", 2400))
-productos.push(new Producto( " Fifa 23 ", 4000))
-productos.push(new Producto( " Lego marvel ", 2400))
-productos.push(new Producto( " UFC ", 2800,))
-productos.push(new Producto( " The Last of Us ", 3100))
-productos.push(new Producto( " R6 ", 4000))
-productos.push(new Producto( " Assasin´s Creed", 4000))
-productos.push(new Producto( " Red Dead Redemption 2", 3800))
-productos.push(new Producto( "GTA", 3000))
+// class Producto{
+//     constructor(nombre,precio,){
+//         this.nombre = nombre
+//         this.precio = parseInt(precio)
+//     }
+//     enviar(){
+//         this.precio = this.precio + 210
+//     }
+//     pago(){
+//         this.precio = this.precio - 500
+//     }
+// }
+// const productos = []
+// productos.push(new Producto( " COD ", 2400))
+// productos.push(new Producto( " Fifa 23 ", 4000))
+// productos.push(new Producto( " Lego marvel ", 2400))
+// productos.push(new Producto( " UFC ", 2800,))
+// productos.push(new Producto( " The Last of Us ", 3100))
+// productos.push(new Producto( " R6 ", 4000))
+// productos.push(new Producto( " Assasin´s Creed", 4000))
+// productos.push(new Producto( " Red Dead Redemption 2", 3800))
+// productos.push(new Producto( "GTA", 3000))
 
-console.log(productos)
-// Filtro
-const PreciosBaratos = productos.filter((el) => el.precio > 3000)
-console.log(PreciosBaratos)
-const PreciosCaros = productos.filter((el) => el.precio < 3100)
-console.log(PreciosCaros)
-// Costo de los productos de consolas y controles
+// console.log(productos)
+// // Filtro
+// const PreciosBaratos = productos.filter((el) => el.precio > 3000)
+// console.log(PreciosBaratos)
+// const PreciosCaros = productos.filter((el) => el.precio < 3100)
+// console.log(PreciosCaros)
+// // Costo de los productos de consolas y controles
 
-let prod1 = 17.000
-let prod2 = 180.000
-let prod3 = 220.000
-let prod4 = 17.000
-let prod5 = 210.000
-let prod6 = 300.000
+// let prod1 = 17.000
+// let prod2 = 180.000
+// let prod3 = 220.000
+// let prod4 = 17.000
+// let prod5 = 210.000
+// let prod6 = 300.000
 
-let total2
-
-
-const prods = [{
-    prod: "Control Playstation 4",
-    precio: 17000
-    },
-    {
-        prod: "Playstation 4",
-        precio: 180000
-    },
-    {
-        prod: "Nintendo Switch",
-        precio: 220000
-    },
-    {
-        prod: "Control Xbox One",
-        precio: 17000
-    },
-    {
-        prod: "Xbox One",
-        precio: 210000
-    },
-    {
-        prod: "Consola Arcade",
-        precio: 300000
-    }
-]
+// let total2
 
 
-
-total2 = 0
-function consolas() {
-while (true){
-    let opcion = parseInt(prompt( " ¿Que consola o control desea comprar? Elige un número:(Si no desea nada presione 0) \n\
-    1-Control playstation 4 \n\
-    2-Playstation 4 \n\
-    3-Nintendo Switch \n\
-    4-Control Xbox One \n\
-    5-Xbox One \n\
-    6-Consola Arcade"))
-    if (opcion >= 1 && opcion <= 6) {
-        let cantidad = parseInt(prompt(`Cuantos ${prods[opcion - 1].prod} desea comprar`))
-        let subtotal = cantidad * prods[opcion - 1].precio
-        total2 += subtotal
-        alert( "Tu costo total es de " + (total2 + total1))
-        break;
-    }else if (opcion == 0){
-        break;
-    }
-}
-}
-consolas()
-
-let totalfinal1 = (total1 + total2)
-console.log(totalfinal1)
-console.log(prods)
-
-
-
-// // Medios de pago
-let total3 = 0
-let envio
-do {
-    envio = prompt(" ¿Lo retiras en tienda? Habra una suma de 210$ si lo llevamos a su domicilio (Si/No) ")
-    if (envio != "si" && envio != "SI" && envio != "Sí" && envio != "NO" && envio != "No" && envio != "no") {
-        alert( " No pusiste lo especificado ")
-    }else{
-        if (envio == "si" || envio == "SI" || envio == "Si" ) {
-            totalfinal1 = totalfinal1
-        }else{
-            totalfinal1 = totalfinal1 + 210
-            for (const producto of productos) {
-                producto.enviar()
-            }
-        }
-    }
-} while (envio != "si" && envio != "SI" && envio != "Si" && envio != "NO" && envio != "No" && envio != "no");
-
-console.log(envio)
-
-let pagar
-do{
-    pagar = prompt( " ¿Pagas en efectivo? Hay un descuento de 500$ en efectivo (Si/No) ")
-    if (pagar != "si" && pagar != "SI" && pagar != "Sí" && pagar != "NO" && pagar != "No" && pagar != "no" ) {
-        alert(" No pusiste lo especificado ")
-    }else{
-        if (pagar == "si" || pagar == "SI" || pagar == "Si" ) {
-            totalfinal1 = totalfinal1 - 500
-        }else{
-            totalfinal1 = totalfinal1
-            for (const producto of productos) {
-                producto.pago()
-            }
-        }
-    }
-}while(pagar != "si" && pagar != "SI" && pagar != "Si" && pagar != "NO" && pagar != "No" && pagar != "no")
-
-console.log (pagar)
-console.log(totalfinal1)
-alert(" tu nuevo costo es de " + totalfinal1)
+// const prods = [{
+//     prod: "Control Playstation 4",
+//     precio: 17000
+//     },
+//     {
+//         prod: "Playstation 4",
+//         precio: 180000
+//     },
+//     {
+//         prod: "Nintendo Switch",
+//         precio: 220000
+//     },
+//     {
+//         prod: "Control Xbox One",
+//         precio: 17000
+//     },
+//     {
+//         prod: "Xbox One",
+//         precio: 210000
+//     },
+//     {
+//         prod: "Consola Arcade",
+//         precio: 300000
+//     }
+// ]
 
 
 
+// total2 = 0
+// function consolas() {
+// while (true){
+//     let opcion = parseInt(prompt( " ¿Que consola o control desea comprar? Elige un número:(Si no desea nada presione 0) \n\
+//     1-Control playstation 4 \n\
+//     2-Playstation 4 \n\
+//     3-Nintendo Switch \n\
+//     4-Control Xbox One \n\
+//     5-Xbox One \n\
+//     6-Consola Arcade"))
+//     if (opcion >= 1 && opcion <= 6) {
+//         let cantidad = parseInt(prompt(`Cuantos ${prods[opcion - 1].prod} desea comprar`))
+//         let subtotal = cantidad * prods[opcion - 1].precio
+//         total2 += subtotal
+//         alert( "Tu costo total es de " + (total2 + total1))
+//         break;
+//     }else if (opcion == 0){
+//         break;
+//     }
+// }
+// }
+// consolas()
 
+// let totalfinal1 = (total1 + total2)
+// console.log(totalfinal1)
+// console.log(prods)
+
+
+
+// // // Medios de pago
+// let total3 = 0
+// let envio
+// do {
+//     envio = prompt(" ¿Lo retiras en tienda? Habra una suma de 210$ si lo llevamos a su domicilio (Si/No) ")
+//     if (envio != "si" && envio != "SI" && envio != "Sí" && envio != "NO" && envio != "No" && envio != "no") {
+//         alert( " No pusiste lo especificado ")
+//     }else{
+//         if (envio == "si" || envio == "SI" || envio == "Si" ) {
+//             totalfinal1 = totalfinal1
+//         }else{
+//             totalfinal1 = totalfinal1 + 210
+//             for (const producto of productos) {
+//                 producto.enviar()
+//             }
+//         }
+//     }
+// } while (envio != "si" && envio != "SI" && envio != "Si" && envio != "NO" && envio != "No" && envio != "no");
+
+// console.log(envio)
+
+// let pagar
+// do{
+//     pagar = prompt( " ¿Pagas en efectivo? Hay un descuento de 500$ en efectivo (Si/No) ")
+//     if (pagar != "si" && pagar != "SI" && pagar != "Sí" && pagar != "NO" && pagar != "No" && pagar != "no" ) {
+//         alert(" No pusiste lo especificado ")
+//     }else{
+//         if (pagar == "si" || pagar == "SI" || pagar == "Si" ) {
+//             totalfinal1 = totalfinal1 - 500
+//         }else{
+//             totalfinal1 = totalfinal1
+//             for (const producto of productos) {
+//                 producto.pago()
+//             }
+//         }
+//     }
+// }while(pagar != "si" && pagar != "SI" && pagar != "Si" && pagar != "NO" && pagar != "No" && pagar != "no")
+
+// console.log (pagar)
+// console.log(totalfinal1)
+// alert(" tu nuevo costo es de " + totalfinal1)
+
+// Agregar productos al carrito
+// let boton = document.getElementById("BtnCarrito1")
+// boton.addEventListener("click", respuestaclick)
+// function respuestaclick() {
+//     console.log("Respuesta click")
+// }
+// let boton1 = document.getElementById("prueba")
+// boton.addEventListener("click",nose)
+// function nose(){
+//     console.log("nose")
+// }
